53d3739de12026f35c20e219416181454e720863
CaulyKan <Cauliflower.kan@gmail.com>
2016-11-09 11:08:30 +0800

    Add TestCollectionChangedView to Testapp

---
+++ TestApp/Tabs/TestCollectionChanged.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestApp
+{
+    /// <summary>
+    /// TestCollectionChanged.xaml 的交互逻辑
+    /// </summary>
+    public partial class TestCollectionChanged : UserControl
+    {
+        public TestCollectionChanged()
+        {
+            InitializeComponent();
+            _tree.Model = new TestCollectionChangedModel();
+        }
+
+        private void _tree_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var a = _tree.Model;
+            _tree.Model = null;
+            _tree.Model = a;
+        }
+    }
+}
