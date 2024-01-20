1f427fd2a607b3d6ae201158c99e6f778cfb4344
Hisashi Horikawa (堀川 久) <hisashi.horikawa@gmail.com>
2024-01-20 21:30:28 +0900

    fix compiling error.

--- Aga.Controls/AssemblyInfo.cs
+++ Aga.Controls/AssemblyInfo.cs
@@ -1,3 +1,5 @@
+﻿
+using System.Runtime.CompilerServices;
 using System.Windows;
 
 [assembly: ThemeInfo(
@@ -8,3 +10,7 @@ using System.Windows;
                                                 //(used if a resource is not found in the page,
                                                 // app, or any theme specific resource dictionaries)
 )]
+
+// プロジェクトを跨ぐ UnitTests で internal class にアクセスできないエラーが出
+// るのを回避.
+[assembly: InternalsVisibleTo("UnitTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100d38e92f544ec55348a59865691be10df252e1e66a18a0260fdcae90866d35e135a0ba34b7c06ee93f56a7eac77cdefce7a10610e4d21d3cd83037b169cc9988e5995efbdb39ccfc42cbe6e0dbde648cb4c27d92742bda0bb293ab924b35d6c2d314d1dee9e8b7752e8aeb0ff27e6bc60817556e650f7b0d8a9e84a155fbd64ab")]
